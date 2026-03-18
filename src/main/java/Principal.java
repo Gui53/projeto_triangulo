625b8e9de5d800dd90ded4b5f3bc9132a393136f
Gui53 <gui53tube@gmail.com>
2026-03-17 21:14:35 -0300

    Fix: comentários

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,14 +1,20 @@
+
 import javax.swing.JOptionPane;
 
 public class Principal {
 
     public static void main(String[] args) {
+        //Variáveis
         double base, altura, area;
 
+        //entrada
         base = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite a base:"));
         altura = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite a altura:"));
-        area = (base*altura)/2.0;
-        
-        JOptionPane.showMessageDialog(null, "A area é " + area);
+
+        //Processamento
+        area = (base * altura) / 2.0;
+
+        //Saída
+        JOptionPane.showMessageDialog(null, "A area é: " + area);
     }
 }
